162853034e61d6d825d303a95afe97a76baa17ff
kevinxsu <kevinsu@college.harvard.edu>
2022-12-10 21:37:54 -0500

    volume knob

--- src/1080final.cpp
+++ src/1080final.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+constexpr double pi = 3.14159265358979323846;
+
 
 struct _1080final : Module {
 	enum ParamId {
@@ -36,6 +38,48 @@ struct _1080final : Module {
         return std::exp(potentiometer) * std::cbrt(-10 * pitch);
     }
 
+    float square_wave(float pitch, int iterations) {
+        float t = std::asin(pitch);
+        //float omega = 2 * pi * pitch;
+        //float omega = 2 * pi * t;
+
+        float result = 0;
+        int constant = 1;
+        for (int i = 0; i < iterations; i++) {
+            result += std::sin(constant * t);
+            constant += 2;
+        }
+
+        return result * 4 / pi;
+    }
+
+    // volume knob
+    float compressor_pedal(float pitch, float potentiometer) {
+        // original?
+        //if (pitch >= 2.5 || pitch <= -2.5) {
+        //if (pitch >= 0) {
+        //    return pitch - potentiometer;
+        //} else {
+        //    return pitch + potentiometer;
+        //}
+
+        return pitch * (1 - potentiometer / 1);
+            
+        //if (pitch >= 2.5) {
+        //    //return pitch * 0.75 + std::cos(pitch);
+        //    return 2.5 - potentiometer + 0.5;
+        //}
+        //else if (pitch <= -2.5) {
+        //    return -2.5 + potentiometer - 0.5;
+        //} else {
+        //    return pitch;
+        //}
+    }
+
+    float boost_effect_pedals(float pitch, float loudness) {
+        return pitch * loudness;
+    }
+
 	void process(const ProcessArgs& args) override {
 		//get potentiometer value
 		float potentiometer = params[POTENTIO_PARAM].getValue();
@@ -46,7 +90,10 @@ struct _1080final : Module {
         //output_signal *= std::cos(potentiometer) + std::exp(pitch);
         //output_signal *= std::exp(std::exp(potentiometer) + std::exp(pitch));
         //output_signal = std::log(output_signal);
-        outputs[OUTPUT].setVoltage(electra(potentiometer, pitch));
+
+        //outputs[OUTPUT].setVoltage(electra(potentiometer, pitch));
+        //outputs[OUTPUT].setVoltage(boost_effect_pedals(pitch, 5));
+        outputs[OUTPUT].setVoltage(compressor_pedal(pitch, potentiometer));
 	}
 };
 
