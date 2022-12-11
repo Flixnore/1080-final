dd6a4214400a223c8189d9c0dd33bb1c981fe3bb
Jared Ni <milkteadj@gmail.com>
2022-12-11 01:59:55 -0500

    added code for volume knob

---
+++ src/volumeknob.cpp
@@ -0,0 +1,52 @@
+#include "plugin.hpp"
+
+
+struct Volumeknob : Module {
+	enum ParamId {
+		BOOST_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		INPUT_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		OUTPUT_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		LIGHTS_LEN
+	};
+
+	Volumeknob() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(BOOST_PARAM, 0.f, 1.f, 0.f, "");
+		configInput(INPUT_INPUT, "");
+		configOutput(OUTPUT_OUTPUT, "");
+	}
+
+	void process(const ProcessArgs& args) override {
+	}
+};
+
+
+struct VolumeknobWidget : ModuleWidget {
+	VolumeknobWidget(Volumeknob* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/volumeknob.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(24.349, 86.989)), module, Volumeknob::BOOST_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(9.642, 114.972)), module, Volumeknob::INPUT_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(39.874, 115.158)), module, Volumeknob::OUTPUT_OUTPUT));
+	}
+};
+
+
+Model* modelVolumeknob = createModel<Volumeknob, VolumeknobWidget>("volumeknob");
